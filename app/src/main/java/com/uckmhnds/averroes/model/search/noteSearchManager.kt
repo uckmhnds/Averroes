b913ec98b45b6dd25f9b2f13059d7ef3d5fa5ab8
uckmhnds <uckmhnds@hotmail.com>
2022-09-09 15:45:28 +0300

    This is the first version of Averroes.
    - It is developed with the help of MVVM pattern
    - It is an offline app
    - It uses ROOM database for storing notes and to-dos in local storage
    - It has recycler view and adapter (with onItemClickListeners)
    - Communication with repo is by sharedViewModels
    - A dialogFragment is implemented to Add Note Fragment
    - A recyclerView callbacks events to the dialogFragment via a interface
    - The dialogFragment callbacks events to the host fragment via a interface
    - Thus, recyclerView shows categories of a note with a dialog screen
    - User selects a category from the dialog and such dialog appears on the add note fragment
    - The selected category is reflected to entity when saved to local
    - Shared ViewModels used to pass data between fragments
    - Shared ViewModels used to access data from main single activity
    - One single AppDatabase is created since two of different named database initialization is totally wrong approach
    - Create a Database Class and define different Daos inside
    - sharedViewModel is used between Activity and Fragment to listen events on topAppBar, then reflects the event on UI (list/grid view)
    - Some lambda functions are moved into private member functions to have fancy and clear code
    - SearchView widget is replaced with EditText
    - searchQueryListener is implemented to SearchView
    - Search Queries are stored at ViewModel
    - ViewModel takes queries from repo (this part should be checked)
    - Search queries are shown at NotesFragment.kt via another RecyclerView
    - The search query display logic is on UI layer
    
    In future:
    
    - UI state will be saved in local for better user-experience
    - CoordinatorLayout will be implemented
    - Scroll view with animation will be added
    - UI layer will be separated from logics (i.e. searchQueryListener(), observeNotes(), observeRecyclerViewLayoutManagerState() etc.)
    - Logics at UI layer will be pushed behind sharedViewModels
    - UI wil be improved with animations
    - Better material design will be implemented
    - Share options will be added
    - Ability to attach pictures (taken from Gallery) option will be added
    - Reminder will be added

---
+++ app/src/main/java/com/uckmhnds/averroes/model/search/noteSearchManager.kt
@@ -0,0 +1,10 @@
+package com.uckmhnds.averroes.model.search
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+
+class noteSearchManager(context: Context, coroutineScope: CoroutineScope) {
+
+
+
+}
---
+++ app/src/main/java/com/uckmhnds/averroes/viewmodel/SearchViewModel.kt
@@ -0,0 +1,11 @@
+package com.uckmhnds.averroes.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.uckmhnds.averroes.databinding.FragmentNotesBinding
+
+class SearchViewModel(binding: FragmentNotesBinding): ViewModel() {
+
+//    val searchText: LiveData<String>    = binding.etSearch.text
+
+}
